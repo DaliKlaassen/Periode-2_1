cf92214b1676fdc2ca99b0ca521d8e236222260d
Dalí <dalimklaassen7@gmail.com>
2017-11-15 19:32:18 +0100

    Add FRO ES6 Week 2 assignment

---
+++ FRO ES6 Week 2/index.js
@@ -0,0 +1,107 @@
+// Maandag 06-11-2017//
+
+let speaker = {
+  brand: "Sonos",
+  function: "Alarm/Music",
+  usesperday: 8
+};
+
+let phone = {
+  brand: "Samsung",
+  funtion: "Social Media",
+  usesperday: "more then " + 100
+};
+
+let bicycle = {
+  brand: "Batavus",
+  function: "Travel",
+  usesperday: "minimal " + 2
+};
+
+// Dinsdag 07-11-2017//
+
+let keyboard = {
+  brand: "SteelSeries",
+  model: "Apex M800",
+  layout: "qwerty",
+  connection: "wired",
+  programmableKeys: "Full, every key re-mappable",
+  rgb: true
+};
+
+let headset = {
+  brand: "Steelseries",
+  model: "Arctis 5 RGB",
+  connection: "wired"
+  rgb: true
+ };
+
+let mouse = {
+  brand: "Steelseries",
+  model: "Rival 300",
+  connection: "Wired",
+  rgb: true
+};
+
+// Woensdag 08-11-2017//
+
+let car = {
+  brand: "Renault",
+  model: "Megané Coupé",
+  drivingLicense: true;
+};
+
+let bed = {
+  brand: "Ikea",
+  length: "2,00m",
+  width: "1.60m"
+};
+
+let laptop = {
+  brand: "Asus",
+  screenSize: "17 Inch",
+  videoCard: "960M 2GB",
+  rgb: false
+};
+
+// Donderdag 09-11-2017//
+
+let earplugs = {
+  brand: "Sony",
+  model: "Extra Bass",
+  connection: "Wired"
+}:
+
+let train = {
+  company: "NS",
+  model: "SLT",
+  travelTime: 6 + " Minutes"
+};
+
+let bus = {
+  company: "GVB",
+  model: "Standard Bus",
+  travelTime: 8 + " Minutes"
+};
+
+// Vrijdag 10-11-2017 //
+
+let bicycle {
+  company: "Thuisbezorgd.nl",
+  brand: "Bike4Delevery",
+  work: true
+}
+
+let tram {
+  company: "GVB".
+  model: "Standard Tram",
+  travelTime: 15 + " minutes"
+}
+
+let money {
+  amount: 2 + "euro",
+  tip: true,
+  spend: true
+}
+
+// Weekend //
